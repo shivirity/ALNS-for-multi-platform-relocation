82b8ff1ad9a70a269ebeaaf80830511fab6ade44
Lionel SHEN <1154216794@qq.com>
2023-12-09 23:58:09 +0800

    fix: alns.py的参数定义

--- alns.py
+++ alns.py
@@ -3,7 +3,7 @@ from algorithm.ALNS import Problem
 
 def get_relocation_routes(num_of_van: int, van_location: list, van_dis_left: list, van_load: list,
                           c_s: int, c_v: int, cur_t: int, t_p: int, t_f: int, t_roll: int, c_mat, ei_s_arr,
-                          ei_c_arr, esd_arr, x_s_arr, x_c_arr, alpha: float = 10.0, plot: bool = True) -> tuple:
+                          ei_c_arr, esd_arr, x_s_arr, x_c_arr, alpha: float = 10.0, plot: bool = True, mode: str = 'multi') -> tuple:
     """
     to calculate the relocation objective value and information.
 
@@ -25,11 +25,12 @@ def get_relocation_routes(num_of_van: int, van_location: list, van_dis_left: lis
     :param x_c_arr: original number of x_c at planning point
     :param alpha: weight of relocation cost
     :param plot: whether to plot the result
+    :param mode: 'multi' or 'single'
     :return: dict of info
     """
     problem = Problem(num_of_van=num_of_van, van_location=van_location, van_dis_left=van_dis_left, van_load=van_load,
                       c_s=c_s, c_v=c_v, cur_t=cur_t, t_p=t_p, t_f=t_f, t_roll=t_roll, c_mat=c_mat, ei_s_arr=ei_s_arr,
-                      ei_c_arr=ei_c_arr, esd_arr=esd_arr, x_s_arr=x_s_arr, x_c_arr=x_c_arr, alpha=alpha, plot=plot)
+                      ei_c_arr=ei_c_arr, esd_arr=esd_arr, x_s_arr=x_s_arr, x_c_arr=x_c_arr, alpha=alpha, plot=plot, mode=mode)
     # problem.print_log = True
     problem.run()
     problem.plot()
